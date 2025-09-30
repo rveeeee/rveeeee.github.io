000f57ed17e2d0dd2825542afe7d294759c62e92
rveeeee <rvebreo777@gmail.com>
2025-10-01 01:31:09 +0800

    Buttons for Next and Previous remove, added a container for the carousel, not yet finished tools and frameworks

--- script.js
+++ script.js
@@ -20,4 +20,18 @@ document.addEventListener("DOMContentLoaded", function () {
   for (var i = 0; i < sidebarLinks.length; i++) {
     sidebarLinks[i].addEventListener("click", closeSidebar);
   }
+
+  const slides = document.querySelector(".slides");
+  const slideCount = document.querySelectorAll(".slide").length;
+  let index = 0;
+
+  document.getElementById("#next").addEventListener("click", () => {
+    index = (index + 1) % slideCount;
+    slides.style.transform = `translateX(-${index * 100}%)`;
+  });
+
+  document.getElementById("#prev").addEventListener("click", () => {
+    index = (index - 1 + slideCount) % slideCount;
+    slides.style.transform = `translateX(-${index * 100}%)`;
+  });
 });
